1b2a18615e29b0fcccb3586cbf1832573faee2f6
root <root@localhost>
2025-03-17 18:14:27 +0000

    found render python3 location

--- config/application.rb
+++ config/application.rb
@@ -12,21 +12,14 @@ Bundler.require(*Rails.groups)
 
 require 'pycall'
 
-puts "PyCall Python Version: #{PyCall.builtins.eval('import sys; sys.version')}"
-puts "PyCall Python Path: #{PyCall.builtins.eval('import sys; sys.executable')}"
-puts "PyCall Python Site-Packages: #{PyCall.builtins.eval('import sys; print(sys.path)')}"
-puts "PyCall Python Version Info: #{PyCall.builtins.eval('import sys; print(sys.version_info)')}"
-
 module CleoCourseScheduler
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
     # Ensure PyCall uses the correct Python version
-    python_path = `which python3`.strip
-    python_path = '/usr/bin/python3' if python_path.empty?
 
-    PyCall.init(python_path) # Change this if needed (`which python3` in terminal)
+    PyCall.init('/home/render/envwrappers/python3')
 
     # Ensure pdfplumber is available
     begin
