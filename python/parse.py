3532c23c0d10a7ff2cacce9144ab503f45afcf19
root <root@localhost>
2025-03-17 20:11:28 +0000

    adding more print statements to figure out what is going on

--- python/parse.py
+++ python/parse.py
@@ -18,12 +18,14 @@ logger.info(sys.path)  # Log the current Python path for debugging
 
 try:
     import pdfplumber
-    print("pdfplumber is installed and ready to use! - high")
+    logger.info("pdfplumber is installed and ready to use! - high\n\n")
 except ModuleNotFoundError:
     print("pdfplumber not found. Installing...")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "pdfplumber"])
+    subprocess.check_call([sys.executable, "pip", "install", "pdfplumber"])
+    output = subprocess.check_output(["pip", "list"], text=True)
+    logger.info(output)
     import pdfplumber  # Try importing again after installation
-    print("pdfplumber is installed and ready to use! - low")
+    logger.info("pdfplumber is installed and ready to use! - low\n\n")
 
 
 
