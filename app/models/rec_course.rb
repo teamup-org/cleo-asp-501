a17f4be63519428ba49541bbcf67f7ef4e8464dc
cameroncao <camcao@tamu.edu>
2025-04-07 15:18:29 -0500

    degreeplanner rec semester

--- app/models/rec_course.rb
+++ app/models/rec_course.rb
@@ -6,5 +6,15 @@ class RecCourse < ApplicationRecord
     validates :uin, presence: true
     validates :semester, presence: true
 
+    after_destroy :remove_related_stu_course
+
+    private
+
+    def remove_related_stu_course
+      if StudentCourse.exists?(student_id: student.google_id, course_id: course.id)
+        StudentCourse.find_by(student_id: student.google_id, course_id: course.id)&.destroy
+      end
+    end
+
   end
   
